3e1cfd6a1e42d7a1f8159f48c7cff5a1768f02cd
Nicolas Vasquez <n.vasquez10@uniandes.edu.co>
2017-03-09 09:10:11 -0500

    Primer commit...

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Blue from './components/blue';
+import Red from './components/red';
+import Purple from './components/purple';
+import Yellow from './components/yellow';
+import Green from './components/green';
+import White from './components/white';
+import Black from './components/black';
+class App extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state={
+            query:''
+        }
+    }
+    
+    
+  render() {
+    return (
+      <div className="App">
+        <div className="jumbotron">
+        <h1>
+        Seacrh pictures, by color!
+        </h1>
+        <input onChange={event=>{this.queries(event.target.value)}}/>
+        </div>
+        <Blue query={this.state.query}/>
+        <Red query={this.state.query}/>
+        <Purple query={this.state.query}/>
+        <Yellow query={this.state.query}/>
+        <Green query={this.state.query}/>
+        <White query={this.state.query}/>
+        <Black query={this.state.query}/>
+      </div>
+    );
+  }
+}
+
+queries(busca)
+{
+    this.setState(
+    {
+        query:busca
+    });
+}
+
+export default App;
---
+++ src/components/red.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+const URL="localhost:9000/flickr"
+class Red extends Component {
+    constructor(props)
+    {
+        super(props);
+        this.state={
+            color:'red',
+            fotos:[]
+        }
+    }
+    render()
+    {
+        <div>
+            
+        </div>
+    }
+    
+    upDateFotos()
+    {
+        var buscar =this.props.query+' '+this.state.color;
+        console.log('buscar: '+buscar);
+        axios.get(URL+'/'+buscar).then(response => {
+            console.log(response);
+            this.setState({
+              fotos:response.data
+            });
+          });        
+    }
+}
+
+export default App;
